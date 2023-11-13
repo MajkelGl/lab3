b7af6e8b2ed6e0f8f5cac0a8462e9d34ed4a7e53
Michał <michgluacad7@gmail.com>
2023-11-13 12:56:22 +0000

    zadanie 1

---
+++ src/ColorChangeApp.java
@@ -0,0 +1,72 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ColorChangeApp extends JFrame {
+
+    private JTextField colorTextField;
+    private JButton changeColorButton;
+    private JPanel colorPanel;
+
+    public ColorChangeApp() {
+        setTitle("Zmiana koloru");
+        setSize(400, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        colorTextField = new JTextField();
+        changeColorButton = new JButton("Zmień kolor");
+        colorPanel = new JPanel();
+
+        add(colorTextField, BorderLayout.NORTH);
+        add(changeColorButton, BorderLayout.CENTER);
+        add(colorPanel, BorderLayout.SOUTH);
+
+        changeColorButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                changeColor();
+            }
+        });
+    }
+
+    private void changeColor() {
+        String colorName = colorTextField.getText().toLowerCase();
+
+        // Sprawdzanie wprowadzonej nazwy koloru
+        Color newColor;
+        switch (colorName) {
+            case "czerwony":
+                newColor = Color.RED;
+                break;
+            case "zielony":
+                newColor = Color.GREEN;
+                break;
+            case "niebieski":
+                newColor = Color.BLUE;
+                break;
+            case "żółty":
+                newColor = Color.YELLOW;
+                break;
+            case "czarny":
+                newColor = Color.BLACK;
+                break;
+            default:
+                JOptionPane.showMessageDialog(this, "Nieprawidłowa nazwa koloru!", "Błąd", JOptionPane.ERROR_MESSAGE);
+                return;
+        }
+
+        colorPanel.setBackground(newColor);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                ColorChangeApp app = new ColorChangeApp();
+                app.setVisible(true);
+            }
+        });
+    }
+}
